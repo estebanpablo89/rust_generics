2afc0938e7795e291e7ebc64fe17f7ba9b079c4e
Pablo Esteban <estebanpablo89@gmail.com>
2025-03-21 20:58:24 -0500

    any type

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use num_traits::{ToPrimitive, Float};
-// both f32 and f64
+// both f32 or f64
 fn solve2<T:Float>(a:T, b:T) -> f64 {
     let a_f64= a.to_f64().unwrap();
     let b_f64= b.to_f64().unwrap();
@@ -7,8 +7,16 @@ fn solve2<T:Float>(a:T, b:T) -> f64 {
     (a_f64.powi(2) + b_f64.powi(2)).sqrt()
 }
 
-// any type
+// any float
 fn solve3<T:Float, U: Float>(a:T, b:U) -> f64 {
+    let a_f64= a.to_f64().unwrap();
+    let b_f64= b.to_f64().unwrap();
+
+    (a_f64.powi(2) + b_f64.powi(2)).sqrt()
+}
+
+// any type
+fn solve4<T:ToPrimitive, U: ToPrimitive>(a:T, b:U) -> f64 {
     let a_f64= a.to_f64().unwrap();
     let b_f64= b.to_f64().unwrap();
 
